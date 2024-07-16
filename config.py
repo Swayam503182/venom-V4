05b369d880906eb7b9e2bf58f2ee3905ec5e5320
Swayam503182 <swayamduttawhatsapp@gmail.com>
2024-07-17 00:17:42 +0530

    config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6720985475:AAEPz5i_t7kP28qjV8cGMw-YJ_LJxdvfxtU")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("xplms1")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6313839964")
 
 
 
